9eacc93982f9b7f4af083552c5176b202da61bdb
Alan <waalanmusa@gmail.com>
2026-09-24 12:18:54 +0500

    Change my photo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-
+import myPhoto from "/assets/photo_5307553742557226562_y.jpg";
 const profile = {
   name: "Alan",
   role: "Computer Science Student & ML Engineer",
@@ -42,7 +42,7 @@ function Hero() {
         <div className="glow"></div>
         <img
           className="portrait"
-          src="https://github.com/alanmusabek/react-homework/blob/main/img/photo_5307553742557226562_y.jpg"
+          src={myPhoto}
           alt="Professional portrait"
         />
         <div className="floating-card">
